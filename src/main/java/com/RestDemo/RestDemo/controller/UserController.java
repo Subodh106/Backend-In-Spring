8cccfa73a14a8bdf1a43b365b00b803c58bfd829
Subodh106 <subodhloves106@gmail.com>
2026-06-30 13:50:51 +0545

    User Controller

---
+++ src/main/java/com/RestDemo/RestDemo/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.RestDemo.RestDemo.controller;
+import com.RestDemo.RestDemo.dto.CreateUserDto;
+import com.RestDemo.RestDemo.dto.UserDto;
+import com.RestDemo.RestDemo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+    @GetMapping("")
+    public ResponseEntity<List<UserDto>> getAllUser(){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUser());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable String id){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
+    }
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody CreateUserDto createuser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(createuser));
+    }
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@RequestBody CreateUserDto updateUser , @PathVariable String id){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.updateUser(updateUser , id));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable String id){
+        UserService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
