8f53c1b59edb2b33a1fa2200ec178b8c6c937f44
Subodh106 <subodhloves106@gmail.com>
2026-07-01 15:53:59 +0545

    controllers is done

---
+++ src/main/java/com/RestDemo/RestDemo/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.RestDemo.RestDemo.controller;
+
+import com.RestDemo.RestDemo.dto.CreateOrderDto;
+import com.RestDemo.RestDemo.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<CreateOrderDto> createOrder(@RequestBody CreateOrderDto createOrderDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.createOrder(createOrderDto).getBody());
+    }
+}
