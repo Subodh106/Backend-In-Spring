9155231bbfd3134476dea7c940a823799bbfdeb3
Subodh106 <subodhloves106@gmail.com>
2026-07-02 15:54:38 +0545

    some refactor

--- src/main/java/com/RestDemo/RestDemo/controller/OrderController.java
+++ src/main/java/com/RestDemo/RestDemo/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.RestDemo.RestDemo.controller;
 import com.RestDemo.RestDemo.dto.CreateOrderDto;
 import com.RestDemo.RestDemo.dto.OrderDto;
 import com.RestDemo.RestDemo.service.OrderService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/order")
+@AllArgsConstructor
 public class OrderController {
     private final OrderService orderService;
-
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
+//
+//    public OrderController(OrderService orderService) {
+//        this.orderService = orderService;
+//    }
 
     @PostMapping("/")
     public ResponseEntity<OrderDto> createOrder(@RequestBody CreateOrderDto createOrderDto) {
