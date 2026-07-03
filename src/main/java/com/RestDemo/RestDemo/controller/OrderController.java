f138ebfbedf57cf1f3d113a340d29d976ad2d298
Subodh106 <subodhloves106@gmail.com>
2026-07-03 16:15:26 +0545

    Pagination is added to get order of specific user

--- src/main/java/com/RestDemo/RestDemo/controller/OrderController.java
+++ src/main/java/com/RestDemo/RestDemo/controller/OrderController.java
@@ -21,8 +21,20 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.createOrder(createOrderDto, userId));
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteOrder(@RequestParam Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.deleteOrder(id));
+
+    }
+
+    @GetMapping("/paginate")
+    public ResponseEntity<List<OrderDto>> getOrderPaginated(@RequestParam Long userId, @RequestParam int page, @RequestParam int pagesSize, @RequestParam(defaultValue = "asc") String direction, @RequestParam(defaultValue = "name") String sortBy) {
+        return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrderPaginated(userId, page, pagesSize, direction, sortBy));
+    }
+
     @GetMapping
     public ResponseEntity<List<OrderDto>> getOrderByUserId(@PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrderById(userId));
     }
+
 }
--- src/main/java/com/RestDemo/RestDemo/service/OrderService.java
+++ src/main/java/com/RestDemo/RestDemo/service/OrderService.java
@@ -1,5 +1,4 @@
 package com.RestDemo.RestDemo.service;
-
 import com.RestDemo.RestDemo.dto.CreateOrderDto;
 import com.RestDemo.RestDemo.dto.OrderDto;
 import com.RestDemo.RestDemo.dto.UserDto;
@@ -9,6 +8,10 @@ import com.RestDemo.RestDemo.repository.OrderRepository;
 import com.RestDemo.RestDemo.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -47,6 +50,35 @@ public class OrderService {
         });
 
         return orderDto;
+    }
+
+    public String deleteOrder(Long id) {
+        Order order = orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        orderRepository.deleteById(order.getID());
+        return "Order deleted Successfully";
+    }
+
+    public List<OrderDto> getOrderPaginated(Long userID, int page, int pageSize, String direction, String sortBy) {
+        Sort sort;
+        sort = direction.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+        Page<Order> orderPage = orderRepository.findOrdersByUserId(userID, pageable);
+        List<OrderDto> orderDtos = new ArrayList<>();
+        orderPage.forEach(order -> {
+            orderDtos.add(
+                    new OrderDto(
+                            order.getID()
+                            , order.getProductName()
+                            , order.getPrice()
+                            , new UserDto(
+                            order.getUser().getId(),
+                            order.getUser().getName(),
+                            order.getUser().getEmail()
+                    )
+                    )
+            );
+        });
+        return orderDtos;
     }
 
 }
