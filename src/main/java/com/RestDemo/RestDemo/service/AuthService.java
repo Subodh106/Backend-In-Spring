7dd71f02f6b44de3e57ec7e4832d4d415d6191f4
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:59:50 +0545

    Some new methods in auth service

--- src/main/java/com/RestDemo/RestDemo/service/AuthService.java
+++ src/main/java/com/RestDemo/RestDemo/service/AuthService.java
@@ -1,22 +1,39 @@
 package com.RestDemo.RestDemo.service;
 
 import com.RestDemo.RestDemo.dto.CreateUserDto;
+import com.RestDemo.RestDemo.dto.LoginDto;
+import com.RestDemo.RestDemo.dto.LoginResponseDto;
 import com.RestDemo.RestDemo.dto.RegisterUserResponseDto;
 import com.RestDemo.RestDemo.entities.Role;
 import com.RestDemo.RestDemo.entities.User;
 import com.RestDemo.RestDemo.repository.UserRepository;
+import com.RestDemo.RestDemo.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    @Autowired
+    public AuthService(PasswordEncoder passwordEncoder, UserRepository userRepository, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
+
 
     public RegisterUserResponseDto RegisterUser(CreateUserDto createUser) {
         User user = new User();
@@ -27,5 +44,13 @@ public class AuthService {
         user.setRole(Role.USER);
         User savedUser = userRepository.save(user);
         return new RegisterUserResponseDto(savedUser.getId(), savedUser.getName());
+    }
+
+    public LoginResponseDto LogInUser(LoginDto login) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(login.getEmail(), login.getPassword()
+                ));
+        String JwtToken = jwtService.generateJwtToken((UserDetails) Objects.requireNonNull(authentication.getPrincipal()));
+        return new LoginResponseDto(JwtToken);
     }
 }
