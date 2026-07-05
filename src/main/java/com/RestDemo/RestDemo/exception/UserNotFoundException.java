dd5be07ad48e2c6398c54ac37610fef23687b5ae
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:48:13 +0545

    Exceptional handling

--- src/main/java/com/RestDemo/RestDemo/exception/UserNotFoundException.java
+++ src/main/java/com/RestDemo/RestDemo/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.RestDemo.RestDemo.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message, Long id) {
+    public UserNotFoundException(String message) {
         super(message);
     }
 }
