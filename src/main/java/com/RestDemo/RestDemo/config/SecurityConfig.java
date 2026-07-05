57545e9bdbba16ddfbd9f80109cd7a533315c8a0
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:46:39 +0545

    Spring security is configured and some role based accessed is also implemented

--- src/main/java/com/RestDemo/RestDemo/config/SecurityConfig.java
+++ src/main/java/com/RestDemo/RestDemo/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.RestDemo.RestDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,7 +21,9 @@ public class SecurityConfig {
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("api/v1/auth/**").permitAll()
-                                .requestMatchers("/api/v1/users/").authenticated()
+                                .requestMatchers("/api/v1/users/").hasRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/v1/users/**").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/users/allUsers").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/users/*/orders/**").authenticated());
         return httpSecurity.build();
     }
@@ -26,5 +31,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) {
+        return authConfig.getAuthenticationManager();
+
     }
 }
