105004ca3ee151aad91232075b9d7d33424eb1ec
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:47:30 +0545

    Login response dto is created

---
+++ src/main/java/com/RestDemo/RestDemo/dto/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.RestDemo.RestDemo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String jwt;
+}
