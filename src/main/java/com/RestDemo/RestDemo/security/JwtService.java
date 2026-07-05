0dcbc0a5935d5e1cef0d76f589b67dedf4aff2ef
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:59:26 +0545

    Service related to jwt

---
+++ src/main/java/com/RestDemo/RestDemo/security/JwtService.java
@@ -0,0 +1,41 @@
+package com.RestDemo.RestDemo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private SecretKey getKey() {
+        String jwtSecret = "JwtSecret";
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateJwtToken(UserDetails userDetails) {
+        return Jwts.builder().subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public Claims extractClaims(String Token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(Token)
+                .getPayload();
+    }
+
+    public boolean isTokenValid(String jwt, UserDetails userDetails) {
+        String email = extractClaims(jwt).getSubject();
+        return email.equals(userDetails.getUsername());
+    }
+}
