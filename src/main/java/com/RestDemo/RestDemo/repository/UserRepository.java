e997884acf94f7a4bbe14da4fcf500c489051c03
Subodh106 <subodhloves106@gmail.com>
2026-07-05 15:48:21 +0545

    Exceptional handling

--- src/main/java/com/RestDemo/RestDemo/repository/UserRepository.java
+++ src/main/java/com/RestDemo/RestDemo/repository/UserRepository.java
@@ -7,9 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Override
     @NullMarked
     Page<User> findAll(Pageable pageable);
+
+    Optional<User> findByEmail(String email);
 }
